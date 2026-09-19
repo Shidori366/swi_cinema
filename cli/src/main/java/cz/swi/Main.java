569920753e56b222ef08d5381f5656abeaedafe1
Shidori366 <d.m.shidori@gmail.com>
2026-09-19 19:14:51 +0200

    added lanterna and sample code

--- cli/src/main/java/cz/swi/Main.java
+++ cli/src/main/java/cz/swi/Main.java
@@ -1,7 +1,25 @@
 package cz.swi;
 
+import com.googlecode.lanterna.TerminalPosition;
+import com.googlecode.lanterna.terminal.DefaultTerminalFactory;
+import com.googlecode.lanterna.terminal.Terminal;
+
 public class Main {
     static void main() {
 
+        DefaultTerminalFactory defaultTerminalFactory = new DefaultTerminalFactory();
+
+        try (Terminal terminal = defaultTerminalFactory.createTerminal()) {
+            terminal.enterPrivateMode();
+            terminal.clearScreen();
+            terminal.setCursorPosition(new TerminalPosition(terminal.getTerminalSize().getColumns() / 2 - 3, terminal.getTerminalSize().getRows() / 2));
+
+            terminal.putString("Hello");
+            terminal.flush();
+
+            Thread.sleep(5000);
+        } catch (Exception e) {
+            System.out.println(e.getMessage());
+        }
     }
 }
